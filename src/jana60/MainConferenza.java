a44d6fe3d860500f1137428b4da4db7e6649429b
matteocovetti <covetti.matteo@gmail.com>
2022-07-01 15:20:08 +0200

    add mileston 3

--- src/jana60/MainConferenza.java
+++ src/jana60/MainConferenza.java
@@ -8,6 +8,7 @@ public class MainConferenza {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
+		boolean errore = true;
 		boolean flag = true;
 		boolean flag2 = true;
 		boolean conf = true;
@@ -28,70 +29,76 @@ public class MainConferenza {
 		} while (flag2);
 		switch (sceltaConfEv) {
 		case "1":
-			try {
-				System.out.print("Inserisci il titolo dell'evento: ");
-				String titolo = scan.nextLine();
+			do {
+				try {
+					System.out.print("Inserisci il titolo dell'evento: ");
+					String titolo = scan.nextLine();
 
-				System.out.println("Quando sarà l'evento?");
-				System.out.print("Giorno: ");
-				int giorno = Integer.parseInt(scan.nextLine());
-				System.out.print("Mese: ");
-				int mese = Integer.parseInt(scan.nextLine());
-				System.out.print("Anno: ");
-				int anno = Integer.parseInt(scan.nextLine());
-				LocalDate dataEvento = LocalDate.of(anno, mese, giorno);
+					System.out.println("Quando sarà l'evento?");
+					System.out.print("Giorno: ");
+					int giorno = Integer.parseInt(scan.nextLine());
+					System.out.print("Mese: ");
+					int mese = Integer.parseInt(scan.nextLine());
+					System.out.print("Anno: ");
+					int anno = Integer.parseInt(scan.nextLine());
+					LocalDate dataEvento = LocalDate.of(anno, mese, giorno);
 
-				System.out.print("Inserisci numero di posti disponibili per l'evento: ");
-				int postiTotali = Integer.parseInt(scan.nextLine());
+					System.out.print("Inserisci numero di posti disponibili per l'evento: ");
+					int postiTotali = Integer.parseInt(scan.nextLine());
 
-				System.out.print("Inserisci l'argomento: ");
-				String argomento = scan.nextLine();
+					System.out.print("Inserisci l'argomento: ");
+					String argomento = scan.nextLine();
 
-				System.out.println("Chi sarà l'oratore?");
-				System.out.print("Nome: ");
-				String nome = scan.nextLine();
-				System.out.print("Cognome: ");
-				String cognome = scan.nextLine();
-				System.out.print("Titolo: ");
-				String titoloOratore = scan.nextLine();
+					System.out.println("Chi sarà l'oratore?");
+					System.out.print("Nome: ");
+					String nome = scan.nextLine();
+					System.out.print("Cognome: ");
+					String cognome = scan.nextLine();
+					System.out.print("Titolo: ");
+					String titoloOratore = scan.nextLine();
 
-				oratore = new Oratore(nome, cognome, titoloOratore);
-				conferenza = new Conferenza(titolo, dataEvento, postiTotali, argomento, oratore);
-			} catch (NumberFormatException e1) {
-				System.out.println("Valore inserito non valido");
-			} catch (NullPointerException e1) {
-				System.out.println(e1.getMessage());
-			} catch (IllegalArgumentException e1) {
-				System.out.println(e1.getMessage());
-			}
+					oratore = new Oratore(nome, cognome, titoloOratore);
+					conferenza = new Conferenza(titolo, dataEvento, postiTotali, argomento, oratore);
+					errore = false;
+				} catch (NumberFormatException e1) {
+					System.out.println("Valore inserito non valido");
+				} catch (NullPointerException e1) {
+					System.out.println(e1.getMessage());
+				} catch (IllegalArgumentException e1) {
+					System.out.println(e1.getMessage());
+				}
+			} while (errore);
 			break;
 		case "2":
-			try {
-				System.out.print("Inserisci il titolo dell'evento: ");
-				String titolo = scan.nextLine();
+			do {
+				try {
+					System.out.print("Inserisci il titolo dell'evento: ");
+					String titolo = scan.nextLine();
 
-				System.out.println("Quando sarà l'evento?");
-				System.out.print("Giorno: ");
-				int giorno = Integer.parseInt(scan.nextLine());
-				System.out.print("Mese: ");
-				int mese = Integer.parseInt(scan.nextLine());
-				System.out.print("Anno: ");
-				int anno = Integer.parseInt(scan.nextLine());
-				LocalDate dataEvento = LocalDate.of(anno, mese, giorno);
+					System.out.println("Quando sarà l'evento?");
+					System.out.print("Giorno: ");
+					int giorno = Integer.parseInt(scan.nextLine());
+					System.out.print("Mese: ");
+					int mese = Integer.parseInt(scan.nextLine());
+					System.out.print("Anno: ");
+					int anno = Integer.parseInt(scan.nextLine());
+					LocalDate dataEvento = LocalDate.of(anno, mese, giorno);
 
-				System.out.print("Inserisci numero di posti disponibili per l'evento: ");
-				int postiTotali = Integer.parseInt(scan.nextLine());
+					System.out.print("Inserisci numero di posti disponibili per l'evento: ");
+					int postiTotali = Integer.parseInt(scan.nextLine());
 
-				eventoTest = new Evento(titolo, dataEvento, postiTotali);
-				conf = false;
-			} catch (NumberFormatException e) {
-				System.out.println("Errore. Numero inserito non valido");
-				e.printStackTrace();
-			} catch (NullPointerException e) {
-				System.out.println(e.getMessage());
-			} catch (IllegalArgumentException e) {
-				System.out.println(e.getMessage());
-			}
+					eventoTest = new Evento(titolo, dataEvento, postiTotali);
+					errore = false;
+					conf = false;
+				} catch (NumberFormatException e) {
+					System.out.println("Errore. Numero inserito non valido");
+					e.printStackTrace();
+				} catch (NullPointerException e) {
+					System.out.println(e.getMessage());
+				} catch (IllegalArgumentException e) {
+					System.out.println(e.getMessage());
+				}
+			} while (errore);
 			break;
 		}
 

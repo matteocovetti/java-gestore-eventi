d2739885bc48482b12da3e9890f454fedfede337
matteocovetti <covetti.matteo@gmail.com>
2022-07-01 14:56:34 +0200

    fix mileston 2

--- src/jana60/Main.java
+++ src/jana60/Main.java
@@ -8,37 +8,40 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
+		boolean errore = true;
 		boolean flag = true;
 
 		System.out.println("Benvenuto alla creazione di un nuovo evento!");
 
 		Evento eventoTest = null;
-		try {
-			System.out.print("Inserisci il titolo dell'evento: ");
-			String titolo = scan.nextLine();
+		do {
+			try {
+				System.out.print("Inserisci il titolo dell'evento: ");
+				String titolo = scan.nextLine();
 
-			System.out.println("Quando sarà l'evento?");
-			System.out.print("Giorno: ");
-			int giorno = Integer.parseInt(scan.nextLine());
-			System.out.print("Mese: ");
-			int mese = Integer.parseInt(scan.nextLine());
-			System.out.print("Anno: ");
-			int anno = Integer.parseInt(scan.nextLine());
-			LocalDate dataEvento = LocalDate.of(anno, mese, giorno);
+				System.out.println("Quando sarà l'evento?");
+				System.out.print("Giorno: ");
+				int giorno = Integer.parseInt(scan.nextLine());
+				System.out.print("Mese: ");
+				int mese = Integer.parseInt(scan.nextLine());
+				System.out.print("Anno: ");
+				int anno = Integer.parseInt(scan.nextLine());
+				LocalDate dataEvento = LocalDate.of(anno, mese, giorno);
 
-			System.out.print("Inserisci numero di posti disponibili per l'evento: ");
-			int postiTotali = Integer.parseInt(scan.nextLine());
+				System.out.print("Inserisci numero di posti disponibili per l'evento: ");
+				int postiTotali = Integer.parseInt(scan.nextLine());
 
-			eventoTest = new Evento(titolo, dataEvento, postiTotali);
-		} catch (NumberFormatException e) {
-			System.out.println("Errore. Numero inserito non valido");
-			e.printStackTrace();
-		} catch (NullPointerException e) {
-			System.out.println(e.getMessage());
-		} catch (IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-		}
-
+				eventoTest = new Evento(titolo, dataEvento, postiTotali);
+				errore = false;
+			} catch (NumberFormatException e) {
+				System.out.println("Errore. Numero inserito non valido");
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				System.out.println(e.getMessage());
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		} while (errore);
 		do {
 			System.out.println("Inserisca 1 per prenotare un posto, 2 per disdire un posto, 3 per uscire:");
 			String scelta = scan.nextLine();

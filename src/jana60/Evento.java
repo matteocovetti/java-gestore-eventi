a511791339a09cda99aa589159f19ea2a7051b57
matteocovetti <covetti.matteo@gmail.com>
2022-07-01 11:11:48 +0200

    add mileston 1

--- src/jana60/Evento.java
+++ src/jana60/Evento.java
@@ -1,6 +1,7 @@
 package jana60;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Evento {
 
@@ -11,8 +12,14 @@ public class Evento {
 	private int postiPrenotati;
 
 	// costruttore
-	public Evento(String titolo, LocalDate data, int postiTotali) {
+	public Evento(String titolo, LocalDate data, int postiTotali)
+			throws NullPointerException, IllegalArgumentException, Exception {
 		super();
+
+		validaTitolo(titolo);
+		validaData(data);
+		validaPostiTotali(postiTotali);
+
 		this.titolo = titolo;
 		this.data = data;
 		this.postiTotali = postiTotali;
@@ -24,7 +31,8 @@ public class Evento {
 		return titolo;
 	}
 
-	public void setTitolo(String titolo) {
+	public void setTitolo(String titolo) throws NullPointerException {
+		validaTitolo(titolo);
 		this.titolo = titolo;
 	}
 
@@ -32,7 +40,8 @@ public class Evento {
 		return data;
 	}
 
-	public void setData(LocalDate data) {
+	public void setData(LocalDate data) throws IllegalArgumentException {
+		validaData(data);
 		this.data = data;
 	}
 
@@ -42,6 +51,56 @@ public class Evento {
 
 	public int getPostiPrenotati() {
 		return postiPrenotati;
+	}
+
+	// controlli private
+	private void validaData(LocalDate data) throws IllegalArgumentException {
+		if (data.isBefore(LocalDate.now())) {
+			throw new IllegalArgumentException("La data dell'evento non può essere precedente alla data odierna");
+		}
+	}
+
+	private void validaPostiTotali(int postiTotali) throws IllegalArgumentException {
+		if (postiTotali <= 0) {
+			throw new IllegalArgumentException("I posti totali non possono essere minori o uguali a zero");
+		}
+	}
+
+	private void validaTitolo(String titolo) throws NullPointerException {
+		if (titolo == null) {
+			throw new NullPointerException("Il titolo non può essere vuoto");
+		}
+	}
+
+	// metodi
+	public int postiDisponibili() {
+		return postiTotali - postiPrenotati;
+	}
+
+	public int prenota() throws IllegalArgumentException, Exception {
+		validaData(data);
+		if (postiDisponibili() == 0) {
+			throw new Exception("Non ci sono più posti disponibili");
+		} else {
+			return postiPrenotati++;
+		}
+	}
+
+	public int disdici() throws IllegalArgumentException, Exception {
+		validaData(data);
+		if (postiPrenotati == 0) {
+			throw new Exception("Non ci sono prenotazioni da disdire");
+		} else {
+			return postiPrenotati--;
+		}
+	}
+
+	// override toString
+	@Override
+	public String toString() {
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String dataFormattata = this.data.format(df);
+		return dataFormattata + " - " + titolo;
 	}
 
 }

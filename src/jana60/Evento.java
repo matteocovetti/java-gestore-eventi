95cc970c4f8df016baa1e5744e09aadcc68f36eb
matteocovetti <covetti.matteo@gmail.com>
2022-07-01 10:17:19 +0200

    add getter e setter - costruttore

--- src/jana60/Evento.java
+++ src/jana60/Evento.java
@@ -1,5 +1,47 @@
 package jana60;
 
+import java.time.LocalDate;
+
 public class Evento {
+
+	// attributi
+	private String titolo;
+	private LocalDate data;
+	private int postiTotali;
+	private int postiPrenotati;
+
+	// costruttore
+	public Evento(String titolo, LocalDate data, int postiTotali) {
+		super();
+		this.titolo = titolo;
+		this.data = data;
+		this.postiTotali = postiTotali;
+		this.postiPrenotati = 0;
+	}
+
+	// getter e setter
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public int getPostiTotali() {
+		return postiTotali;
+	}
+
+	public int getPostiPrenotati() {
+		return postiPrenotati;
+	}
 
 }
